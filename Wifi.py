d2b50dcea59c9f457fbc6a917d8003cbe06eaf55
Tung-dotcom <tunglam814@gmail.com>
2020-10-26 19:05:14 +0000

    Good

--- Wifi.py
+++ Wifi.py
@@ -1,18 +1,21 @@
 import sys
-import subprocess 
-import os
+import subprocess
+import os 
 from decouple import config
 
-IP_NETWORK = config('IP_NETWORK')
 IP_DEVICE = config('IP_DEVICE')
 
-proc = subprocess.Popen(["ping", "-t", IP_NETWORK],stdout=subprocess.PIPE)
+proc = subprocess.Popen(["ping", "-t", IP_DEVICE], stdout=subprocess.PIPE)
 while True:
-  line = proc.stdout.readline()
-  if not line:
-    break
-  #the real code does filtering here
-  connected_ip = line.decode('utf-8').split()[2]
-
-  if connected_ip == IP_DEVICE:
-      print("Tung's home")
+	line = proc.stdout.readline()
+	if not line:
+		break
+	try:
+		connected_ip = line.decode('utf-8').split()[2].replace(':', '')
+		if connected_ip == IP_DEVICE:
+			print("Tung is home")
+			break
+		else:
+			print("Pinging...")
+	except:
+		pass
