464b294c02b1777a140a4c82922cdbde9d3d7b58
Tung-dotcom <tunglam814@gmail.com>
2020-10-27 18:42:57 +0000

    Functional

--- Wifi.py
+++ Wifi.py
@@ -4,14 +4,14 @@ import os
 from decouple import config
 
 IP_DEVICE = config('IP_DEVICE')
-
-proc = subprocess.Popen(["ping", IP_DEVICE], stdout=subprocess.PIPE)
+proc = subprocess.Popen(["ping","-t", IP_DEVICE], stdout=subprocess.PIPE)
 while True:
 	line = proc.stdout.readline()
 	if not line:
 		break
 	try:
-		connected_ip = line.decode('utf-8').split()[2].replace(':', '')
+		connected_ip = line.decode("utf-8").split()[2].replace(':','')
+		print(connected_ip)
 		if connected_ip == IP_DEVICE:
 			print("Tung is home")
 			break
