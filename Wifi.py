5b0c8bf5473abbe7557b816d57601d48ebe05991
Tung-dotcom <tunglam814@gmail.com>
2020-10-28 10:42:22 +0000

    Added Wifi.py Modified .gitignore

--- Wifi.py
+++ Wifi.py
@@ -1,19 +1,21 @@
 import subprocess
 from decouple import config
+import time
 
 IP_DEVICE = config('IP_DEVICE')
+
 proc = subprocess.Popen(["ping","-t", IP_DEVICE], stdout=subprocess.PIPE)
-while True:
+for i in range(1,10):
 	line = proc.stdout.readline()
 	if not line:
 		break
 	try:
 		connected_ip = line.decode("utf-8").split()[2].replace(':', '')
-		print(connected_ip)
 		if connected_ip == IP_DEVICE:
-			print("Tung is home")
+			print("My phone is in the house")
 			break
 		else:
+			time.sleep(2)
 			print("Pinging...")
 	except:
 		pass
